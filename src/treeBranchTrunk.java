42a529406fc98d50b78be9f86525e05503dc42fa
91085566 <audianreinhard@gmail.com>
2020-03-09 13:23:39 -0700

    Added branch

--- src/treeBranchTrunk.java
+++ src/treeBranchTrunk.java
@@ -3,6 +3,7 @@ public class treeBranchTrunk
     public static void main(String[] args)
     {
         System.out.println("The trees are dying");
+        System.out.println("Mr. Beast planted 20,000,000 trees.");
     }//end main method
-    
+
 }//end driver class
